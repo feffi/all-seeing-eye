bc91882220138eac6ffecd43043c6e31403c21c3
feffi <feffi@feffi.org>
2016-11-10 21:19:41 +0100

    deleted sensors

--- logspout/run-custom.sh
+++
@@ -1,9 +0,0 @@
-set -ex
-
-./build-custom.sh
-docker build --file Dockerfile.custom -t mylogspouter .
-docker run --rm --name=logspout \
-    -v=/var/run/docker.sock:/var/run/docker.sock \
-    -p 8000:80 \
-    mylogspouter \
-    ${SYSLOG}
