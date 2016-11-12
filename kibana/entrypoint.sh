21d8a3f0080c297643ddd6fc7053b43d11098718
feffi <feffi@feffi.org>
2016-11-12 22:26:35 +0100

    fixed config

--- kibana/entrypoint.sh
+++ kibana/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Wait for the Elasticsearch container to be ready before starting Kibana.
-echo "Stalling for Elasticsearch"
-while true; do
-    nc -q 1 elasticsearch 9200 2>/dev/null && break
-done
+#echo "Stalling for Elasticsearch"
+#while true; do
+#    nc -q 1 elasticsearch 9200 2>/dev/null && break
+#done
 
-curl -XPUT 'http://elasticsearch:9200/_template/packetbeat' -d@/tmp/packetbeat.template.json
+#curl -XPUT 'http://elasticsearch:9200/_template/packetbeat' -d@/opt/kibana/config/packetbeat.template.json
 
 echo "Starting Kibana"
 exec kibana
