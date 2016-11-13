d22f04ad294021d2f323fc6269c1cee11caf100f
feffi <feffi@feffi.org>
2016-11-13 13:03:38 +0100

    fixed config

--- packetbeat/entrypoint.sh
+++ packetbeat/entrypoint.sh
@@ -5,10 +5,11 @@
 #while true; do
 #    nc -q 1 elasticsearch 9200 2>/dev/null && break
 #done
-echo "Waiting for elasticsearch"
-./await -f tcp4://elasticsearch:9200
 
 #curl -XPUT 'http://elasticsearch:9200/_template/packetbeat' -d@/opt/kibana/config/packetbeat.template.json
 
-echo "Starting Kibana"
-exec kibana
+echo "Waiting for logstash"
+./await -f tcp4://logstash:5044
+
+echo "Starting PacketBeat"
+exec ./packetbeat -e -c=/etc/packetbeat/packetbeat.yml
